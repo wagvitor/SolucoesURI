ba0b7e1a9df460e94b4b9feb9d3d4aaa909d41b6
wagvitor <w_vitor@yahoo.com.br>
2015-11-24 09:21:11 -0300

    Create 1006.c
    Média 2
    
    Leia 3 valores, no caso variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
    
    Entrada
    O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
    
    Saída
    Imprima a variável MEDIA conforme exemplo abaixo, com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".

---
+++ 1006.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+  
+int main() {
+  double A, B, C, MEDIA;
+ 
+    A, B, C >= 0 && A, B, C <= 10.0;
+ 
+    scanf("%lf",&A);
+    scanf("%lf",&B);
+    scanf("%lf",&C);
+ 
+    if ((A <= 10.0) && (B <= 10.0) && (C <= 10.0))
+    MEDIA = ((A * 2.0) + (B * 3.0) + (C*5.0))/10;
+ 
+ 
+    printf("MEDIA = %.1f\n",MEDIA);
+  
+    return 0;
+}
