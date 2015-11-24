9d88fbfb50bbd4ae5fa30adabdb3011e6a1ebfe2
wagvitor <w_vitor@yahoo.com.br>
2015-11-24 09:09:36 -0300

    Create 1002.c
    Área do círculo
    
    A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando que para este problema que π = 3.14159:
    
    - Efetue o cálculo da área, elevando o valor de Raio ao quadrado e multiplicando por π.
    
    Entrada
    A entrada contém um valor de ponto flutuante (dupla precisão) no caso a variável raio.
    
    Saída
    Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".

---
+++ 1002.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+  
+int main() {
+  
+double A;
+double raio;
+double pi;
+ 
+pi=3.14159;
+ 
+    scanf("%lf",&raio);
+ 
+    A=(raio*raio)*pi;
+    printf("A=%.4lf\n",A);
+    return 0;
+}
